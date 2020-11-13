62bfd0d7fec66db59f1a3932ae5c68edd6b7287f
Michal Rowicki <Michal.Rowicki@vimn.com>
2020-11-13 16:55:57 +0100

    enhanced switch

--- src/main/java/tech/viacomcbs/pandemicspecialstrategies/PandemicSpecialStrategiesApplication.java
+++ src/main/java/tech/viacomcbs/pandemicspecialstrategies/PandemicSpecialStrategiesApplication.java
@@ -18,21 +18,11 @@ class PandemicService {
 
 	void handle(String stimulus) {
 		switch (stimulus) {
-			case "parents":
-				System.out.println("Social distance with parents!");
-				break;
-			case "school":
-				System.out.println("Oh my internet sucks!");
-				break;
-			case "money":
-				System.out.println("What about Pandemic Special?");
-				break;
-			case "health":
-				System.out.println("Wear chin diaper!");
-				break;
-			default:
-				System.out.println("Unexpected behavior. Segmentation fault!");
-				break;
+			case "parents" -> System.out.println("Social distance with parents!");
+			case "school" -> System.out.println("Oh my internet sucks!");
+			case "money" -> System.out.println("What about Pandemic Special?");
+			case "health" -> System.out.println("Wear chin diaper!");
+			default -> System.out.println("Unexpected behavior. Segmentation fault!");
 		}
 	}
 }
