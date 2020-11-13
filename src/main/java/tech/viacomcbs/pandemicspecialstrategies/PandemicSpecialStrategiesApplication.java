407e49dee9a3b3653c5860b095934e0af69fc80d
Michal Rowicki <Michal.Rowicki@vimn.com>
2020-11-13 17:03:48 +0100

    let spring inject strategies

--- src/main/java/tech/viacomcbs/pandemicspecialstrategies/PandemicSpecialStrategiesApplication.java
+++ src/main/java/tech/viacomcbs/pandemicspecialstrategies/PandemicSpecialStrategiesApplication.java
@@ -1,8 +1,12 @@
 package tech.viacomcbs.pandemicspecialstrategies;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class PandemicSpecialStrategiesApplication {
@@ -14,15 +18,54 @@ public class PandemicSpecialStrategiesApplication {
 }
 
 @Service
+@RequiredArgsConstructor
 class PandemicService {
 
+	private final Map<String, BehaviorStrategy> strategies;
+
 	void handle(String stimulus) {
-		switch (stimulus) {
-			case "parents" -> System.out.println("Social distance with parents!");
-			case "school" -> System.out.println("Oh my internet sucks!");
-			case "money" -> System.out.println("What about Pandemic Special?");
-			case "health" -> System.out.println("Wear chin diaper!");
-			default -> System.out.println("Unexpected behavior. Segmentation fault!");
-		}
+		BehaviorStrategy strategy = strategies.get(stimulus);
+		strategy.handle();
+	}
+}
+
+interface BehaviorStrategy {
+
+	void handle();
+}
+
+@Component(value = "parents")
+class ParentsBehavior implements BehaviorStrategy {
+
+	@Override
+	public void handle() {
+		System.out.println("Social distance with parents!");
+	}
+}
+
+@Component(value = "school")
+class SchoolBehavior implements BehaviorStrategy {
+
+	@Override
+	public void handle() {
+		System.out.println("Oh my internet sucks!");
+	}
+}
+
+@Component(value = "money")
+class MoneyBehavior implements BehaviorStrategy {
+
+	@Override
+	public void handle() {
+		System.out.println("What about Pandemic Special?");
+	}
+}
+
+@Component(value = "health")
+class HealthBehavior implements BehaviorStrategy {
+
+	@Override
+	public void handle() {
+		System.out.println("Wear chin diaper!");
 	}
 }
